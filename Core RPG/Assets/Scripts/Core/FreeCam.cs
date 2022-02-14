3c0b5af105b6bc3553e2e3bfd997b85f2962f22a
sdroszel <57762591+sdroszel@users.noreply.github.com>
2022-02-14 14:06:12 -0500

    Project Import

---
+++ Core RPG/Assets/Scripts/Core/FreeCam.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class FreeCam : MonoBehaviour
+{
+
+    [SerializeField] float freeLookSensitivity = 3f;
+    [SerializeField] float minY;
+    [SerializeField] float maxY;
+
+    private bool looking = false;
+
+    void Update()
+    {
+        if (looking)
+        {
+            float newRotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * freeLookSensitivity;
+            float newRotationY = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * freeLookSensitivity;
+            transform.localEulerAngles = new Vector3(Mathf.Clamp(newRotationY, minY, maxY), newRotationX, 0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartLooking();
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            StopLooking();
+        }
+    }
+
+    public void StartLooking()
+    {
+        looking = true;
+    }
+
+    public void StopLooking()
+    {
+        looking = false;
+    }
+}
---
+++ Core RPG/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,53 @@
+
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        [SerializeField] Transform target;
+
+        NavMeshAgent navMeshAgent;
+        Health health;
+
+        private void Start()
+        {
+            health = GetComponent<Health>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            navMeshAgent.enabled = !health.IsDead();
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+
+            GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+    }
+}
